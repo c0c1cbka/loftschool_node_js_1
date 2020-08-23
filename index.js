4e1be390a5183fadf779c5e21bea4867ed889b8e
ANDREI VEREMENKO <C0C1CbKA@gmail.com>
2020-08-23 18:05:31 +0300

    Реализовал сервер и вроде сразу правильно

--- index.js
+++ index.js
@@ -1,95 +1,56 @@
 //Подключаем модули
-const fs = require('fs');
-const path = require('path');
+const express = require('express');
 
+//Задаём переменные сервера
+const app = express();
+const PORT = 8082;
 
-//Определяем путь до исходной папки
-let my_folder_path;
-if (process.argv[2] == undefined) {
-    my_folder_path = './randomFolder';
-} else {
-    my_folder_path = process.argv[2];
+//Проверяем определенны ли переменные окружения
+if (!process.env.nodejs_dz2_limit) {
+    console.error('Переменная окружения nodejs_dz2_limit не обнаруженна. Старт не возможен')
+    exit(1);
+}
+if (!process.env.nodejs_dz2_delay) {
+    console.error('Переменная окружения nodejs_dz2_delay не обнаруженна. Старт не возможен')
+    exit(2);
 }
 
-//Определяем путь до итоговой папки
-let my_new_folder_path;
-if (process.argv[3] == undefined) {
-    my_new_folder_path = './beatifulFolder';
-} else {
-    my_new_folder_path = process.argv[3];
+//Определяем переменные таймеров из переменных окружения и переводим их в милисекунды
+const LIMIT = process.env.nodejs_dz2_limit * 1000;
+const DELAY = process.env.nodejs_dz2_delay * 1000;
+
+//функция вывода времени
+let showTime = (res) => {
+    let datetime = new Date();
+    let datetimeUTC = datetime.toUTCString();
+    res.write(`${datetimeUTC}\n`);
+};
+
+let createAndDestroinInterval = (res) => {
+    //выполняем в первый раз функцию
+    showTime(res);
+
+    //Задаем интервал чтобы функция выполнялась раз в DELAY секунд и сохроняем переменную интервала
+    let interval = setInterval(() => {
+        showTime(res);
+    }, DELAY);
+
+    //Через LIMIT милесикунд удаляем интервал и закрываем соединение
+    setTimeout(() => {
+        clearInterval(interval);
+        res.write("END\n");
+        res.end();
+    }, LIMIT);
 }
 
-//Функция рекурсивного поиска всех файлов
-//возвращает массив объектов 
-function readFolder(folderPath) {
-    //Инициализирую массив
-    let array_obj = [];
-    
-    try{
-    //Читаю синхронно все файлы
-    fs.readdirSync(folderPath, {
-        withFileTypes: true,
-        encoding: 'utf-8'
-    }).forEach(el => {
-        if (el.isDirectory()) {
-            //Если файлы это папка то рекурсивно вызываю этуже функцию
-            readFolder(path.join(folderPath, el.name)).forEach(element => {
-                //прохожусь по результату циклом а кладу в массив
-                array_obj.push(element);
-            });
-        } else {
-            if (el.isFile()) {
-                //если файлы это файл то формирую объект и записываю в массив
-                obj_file = {
-                    "file_name": el.name,//имя файлф
-                    "file_second": el.name[0].toUpperCase(),//первая буква имени файла
-                    "file_path": path.join(folderPath, el.name)//путь до файла
-                }
-                array_obj.push(obj_file);
-            } else {
-                //если файл ни папка и не файлы то ошибка
-                console.error(`${path.join(folderPath,el.name)} - не определенный тип`);
-                exit(2);
-            }
-        }
-    });
-    } catch (err){
-        console.error(err);
-        exit(1);
-    }
+//Обрабатываем все входящие переменные
+app.get('/', (req, res, next) => {
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    res.setHeader('Transfer-Encoding', 'chunked');
+    createAndDestroinInterval(res);
+});
 
-    return array_obj;
-}
-
-//Функция создания папок c наведёным порядком
-function systematizationFiles(systematization_folder_name,array_obj_path){
-    //Проверяем есть ли результирующая папка
-    //если нет то создаём
-    try{
-        fs.accessSync(systematization_folder_name,fs.constants.F_OK);
-    }catch (err){
-        fs.mkdirSync(systematization_folder_name);
-    }
-
-    //цикл по массиву с путями до файла
-    array_obj_path.forEach(el=>{
-        //Проверяем есть ли папка с именем начинающимся на первую букву названия файла
-        //если нет то создаём
-        try{
-            fs.accessSync(path.join(systematization_folder_name,el.file_second),fs.constants.F_OK);
-        }catch (err){
-            fs.mkdirSync(path.join(systematization_folder_name,el.file_second));
-        }
-
-        //Копируем файл в итоговую
-        fs.copyFileSync(el.file_path, path.join(path.join(systematization_folder_name,el.file_second),el.file_name));
-
-    });
-}
-
-try{
-    systematizationFiles(my_new_folder_path,readFolder(my_folder_path));
-    console.log('Ok');
-} catch(err){
-    console.error(err);
-}
+//Стартуем сервер
+app.listen(PORT, () => {
+    console.log(`сервер запущен на порту: ${PORT}`);
+});
